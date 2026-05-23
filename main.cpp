e898b5c94b1f7a7aa1b07812d8dfc8ae4d7ddb67
Chris P <cplanck@my365.bellevue.edu>
2026-05-22 23:08:16 -0500

    init commit

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int apples = 5;
+    int* fruitPtr = nullptr;
+
+    cout << "Apples: " << *fruitPtr << endl;
+
+    bananas = 10;
+
+    int* orangePtr = &oranges;
+
+    cout << "Fruit count: " << apples << endl
+
+    return 0;
+}
